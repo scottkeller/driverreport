3bbf38200d98491b07200cee51ea65d09056af0d
Scott Keller <scottjameskeller@gmail.com>
2018-06-13 15:22:01 -0400

    Added start_time, end_time, and distance to Trip class

--- root-engineering-pkg/root-engineering/core/trip.py
+++ root-engineering-pkg/root-engineering/core/trip.py
@@ -1,5 +1,11 @@
+import datetime
+
+# Constants
+TIME_FORMAT = '%H:%M'
 
 class Trip:
     """Trip object to store driving time and distance"""
-    def __init__(self):
-        pass
+    def __init__(self, start_time, end_time, distance):
+        self.start_time = datetime.datetime.strptime(start_time, TIME_FORMAT)
+        self.end_time = datetime.datetime.strptime(end_time, TIME_FORMAT)
+        self.distance = distance
--- root-engineering-pkg/root-engineering/tests/test_trip.py
+++ root-engineering-pkg/root-engineering/tests/test_trip.py
@@ -1,6 +1,9 @@
 import unittest
 from core.trip import Trip
 
+# Constants
+TIME_FORMAT = '%H:%M'
+
 class TestDriver(unittest.TestCase):
 
     def setUp(self):
@@ -8,6 +11,13 @@ class TestDriver(unittest.TestCase):
 
     def test_trip_exists(self):
         self.assertIsNotNone(self.trip)
+
+    def test_trip_atrrbutes(self):
+        mytrip = self.trip('07:00', '07:50', 39.1)
+        self.assertEqual(mytrip.start_time.time().strftime(TIME_FORMAT), '07:00')
+        self.assertEqual(mytrip.end_time.time().strftime(TIME_FORMAT), '07:50')
+        self.assertEqual(mytrip.distance, 39.1)
+
 
 
 if __name__ == '__main__':
