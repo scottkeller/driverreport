8b06799707b311575ec42d669697158624034446
Scott Keller <scottjameskeller@gmail.com>
2018-06-13 16:06:37 -0400

    Added test for adding multiple trips to a Driver

--- root-engineering-pkg/root-engineering/tests/test_driver.py
+++ root-engineering-pkg/root-engineering/tests/test_driver.py
@@ -22,10 +22,32 @@ class TestDriver(unittest.TestCase):
         self.assertEqual(len(mydriver.trips), 1)
         for trip in mydriver.trips:
             self.assertIsInstance(trip, Trip)
-            self.assertEqual(trip.start_time.time().strftime(TIME_FORMAT), '01:17')
-            self.assertEqual(trip.end_time.time().strftime(TIME_FORMAT), '03:21')
-            self.assertEqual(trip.distance, 133)
-            self.assertEqual(trip.duration, 124)
+        self.assertEqual(mydriver.trips[0].start_time.time().strftime(TIME_FORMAT), '01:17')
+        self.assertEqual(mydriver.trips[0].end_time.time().strftime(TIME_FORMAT), '03:21')
+        self.assertEqual(mydriver.trips[0].distance, 133)
+        self.assertEqual(mydriver.trips[0].duration, 124)
+
+    def test_multiple_trips(self):
+        mydriver = Driver('Scott')
+        mytrips = [
+                    Trip('02:30', '13:54' ,855),
+                    Trip('14:30', '14:31', 1.5),
+                    Trip('11:11', '12:24', 70)
+                ]
+
+        for trip in mytrips:
+            mydriver.add_trip(trip.start_time.strftime(TIME_FORMAT), trip.end_time.strftime(TIME_FORMAT), trip.distance)
+
+        self.assertEqual(len(mydriver.trips), 3)
+
+        for i, trip in enumerate(mydriver.trips):
+            self.assertIsInstance(trip, Trip)
+            self.assertEqual(trip.start_time, mytrips[i].start_time)
+            self.assertEqual(trip.end_time, mytrips[i].end_time)
+            self.assertEqual(trip.distance, mytrips[i].distance)
+            self.assertEqual(trip.duration, mytrips[i].duration)
+
+
 
 
 
