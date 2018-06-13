6b6f650644b369e6cb18e41cd43f035a93bcd94a
Scott Keller <scottjameskeller@gmail.com>
2018-06-13 15:42:51 -0400

    Added add_trip method to Driver class to enable adding trips to the Driver.trips list

--- root-engineering-pkg/root-engineering/core/driver.py
+++ root-engineering-pkg/root-engineering/core/driver.py
@@ -1,6 +1,12 @@
+from .trip import Trip
 
 class Driver:
 
      """Driver object to store name and trip information"""
      def __init__(self, name):
          self.name = name
+         self.trips = []
+
+     def add_trip(self, start_time, end_time, distance):
+         trip = Trip(start_time, end_time, distance)
+         self.trips.append(trip)
--- root-engineering-pkg/root-engineering/tests/test_driver.py
+++ root-engineering-pkg/root-engineering/tests/test_driver.py
@@ -1,5 +1,9 @@
 import unittest
 from core.driver import Driver
+from core.trip import Trip
+
+# Constants
+TIME_FORMAT = '%H:%M'
 
 class TestDriver(unittest.TestCase):
 
@@ -11,6 +15,18 @@ class TestDriver(unittest.TestCase):
 
     def test_driver_name(self):
         self.assertEqual(self.driver('Scott').name, 'Scott')
+
+    def test_add_single_trip(self):
+        mydriver = Driver('Scott')
+        mydriver.add_trip('01:17', '3:21', 133)
+        self.assertEqual(len(mydriver.trips), 1)
+        for trip in mydriver.trips:
+            self.assertIsInstance(trip, Trip)
+            self.assertEqual(trip.start_time.time().strftime(TIME_FORMAT), '01:17')
+            self.assertEqual(trip.end_time.time().strftime(TIME_FORMAT), '03:21')
+            self.assertEqual(trip.distance, 133)
+            self.assertEqual(trip.duration, 124)
+
 
 
 if __name__ == '__main__':
